66a63a192bfac63223d8fb56a56299f1b319a3fd
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-09-11 17:11:00 -0300

    feat: add stylized messages to chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,20 @@ export default function ChatPage() {
           <h2 className="title">Sofia</h2>
           <h2 className="tagline">O seu assistente financeiro</h2>
         </div>
-        <div className="messages-area"></div>
+        <div className="messages-area">
+          <div className="message -bot">
+            Olá, sou a Sofia, como posso te ajudar?
+          </div>
+          <div className="options-group">
+            <button className="option">Relatório</button>
+            <button className="option">Dica</button>
+            <button className="option">Renegociação</button>
+          </div>
+          <div className="message -user">Renegociação</div>
+          <div className="message -bot">
+            Pois bem, para renegociação preciso consultar o fluxograma da Carol.
+          </div>
+        </div>
         <div className="input-area">
           <input className="input" placeholder="Digite aqui sua mensagem" />
           <SendOutlined className="sendbutton" />
