da03170045062f9390a4d2d421e5563b75473e66
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-09-13 14:43:11 -0300

    feat: animate transition between pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import "styles/index.css";
 import "styles/splash.css";
 
-export default function SafraBotApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function SafraBotApp({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
