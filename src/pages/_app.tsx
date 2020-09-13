1074e3620b234d26024e0fdd661fdfa8ec874215
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-09-13 14:39:35 -0300

    feat: add splash screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "styles/index.css";
+import "styles/splash.css";
 
 export default function SafraBotApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
---
+++ src/pages/splash.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { LoadingOutlined } from "@ant-design/icons";
+
+export default function SplashPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => router.push("/"), 2000);
+  }, []);
+
+  return (
+    <div className="splash-page">
+      <img className="avatar" src="sofia.png" />
+      <h1 className="tagline">Sofia</h1>
+      <p className="subtagline">A sua assistente financeira</p>
+      <LoadingOutlined className="spinner" />
+    </div>
+  );
+}
