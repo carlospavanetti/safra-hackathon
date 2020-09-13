c91b0f88c5ab4dc664710847df07ccbec1e6c088
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-09-12 22:33:12 -0300

    refactor: split listeners

--- src/services/Dialog.tsx
+++ src/services/Dialog.tsx
@@ -42,69 +42,81 @@ export default function useDialog() {
     ]);
   }
 
+  function reportListener(msg, ctx) {
+    if (msg === "Relatório") {
+      pushMessage("O seu relatório mensal está aqui", "bot");
+      pushMessage("...[Relatório]", "bot");
+      pushMessage("Quer conferir análises exclusivas?", "bot");
+      setContext("conferir-analises");
+      showOptions(["Sim", "Não"]);
+    }
+  }
+
+  function tipListener(msg, ctx) {
+    if (msg === "Dica" || (msg === "Sim" && ctx === "conferir-analises")) {
+      pushMessage("Estas são as morning calls selecionadas para você:", "bot");
+      pushMessage(
+        "Para outras análises, consulte nossos especialistas 😁",
+        "bot"
+      );
+      showCSat();
+    }
+  }
+
+  async function renegotiationListener(msg, ctx) {
+    if (msg === "Renegociação" || (msg === "Sim" && ctx === "renegociar")) {
+      pushMessage("Vou analisar a sua solicitação", "bot");
+      await waitForMs(1000);
+      pushMessage("Foi aprovada! 😉", "bot");
+      pushMessage("Escolhar uma opção:", "bot");
+      showOptions([
+        "Congelar dívida por 3 meses com juros pré-definidos",
+        "Congelar dívida por 6 meses com juros pré-definidos",
+        "Cancelar",
+      ]);
+    }
+
+    if (msg.indexOf("Congelar dívida") === 0) {
+      pushMessage("Sua dívida foi renegociada!", "bot");
+      pushMessage("O protocolo da solicitação é #1382", "bot");
+      showCSat();
+    }
+
+    if (msg === "Cancelar") {
+      pushMessage("Sua renegociação foi cancelada.", "bot");
+      showCSat();
+    }
+  }
+
+  function satisfactionListener(msg, ctx) {
+    if (msg === "Estou bem, obrigado(a)") {
+      pushMessage("Então até mais, estou por aqui se precisar!", "bot");
+    }
+
+    if (msg.indexOf("Pouco satisfeito") === 0 || msg === "Indiferente") {
+      pushMessage("É uma pena ouvir isso", "bot");
+      pushMessage(
+        "Nossa equipe está trabalhando para que eu possa melhorar nossa interação por aqui.",
+        "bot"
+      );
+      finishChat();
+    }
+
+    if (msg.indexOf("Muito satisfeito") === 0) {
+      finishChat();
+    }
+  }
+
   useEffect(() => {
     pushMessage("Olá, sou a Sofia, como posso te ajudar?", "bot");
     showOptions(["Relatório", "Dica", "Renegociação"]);
-    addListener(async (msg, ctx) => {
-      if (msg === "Relatório") {
-        pushMessage("O seu relatório mensal está aqui", "bot");
-        pushMessage("...[Relatório]", "bot");
-        pushMessage("Quer conferir análises exclusivas?", "bot");
-        setContext("conferir-analises");
-        showOptions(["Sim", "Não"]);
-      }
-      if (msg === "Dica" || (msg === "Sim" && ctx === "conferir-analises")) {
-        pushMessage(
-          "Estas são as morning calls selecionadas para você:",
-          "bot"
-        );
-        pushMessage(
-          "Para outras análises, consulte nossos especialistas 😁",
-          "bot"
-        );
-        showCSat();
-      }
-      if (msg === "Renegociação" || (msg === "Sim" && ctx === "renegociar")) {
-        pushMessage("Vou analisar a sua solicitação", "bot");
-        await waitForMs(1000);
-        pushMessage("Foi aprovada! 😉", "bot");
-        pushMessage("Escolhar uma opção:", "bot");
-        showOptions([
-          "Congelar dívida por 3 meses com juros pré-definidos",
-          "Congelar dívida por 6 meses com juros pré-definidos",
-          "Cancelar",
-        ]);
-      }
 
-      if (msg.indexOf("Congelar dívida") === 0) {
-        pushMessage("Sua dívida foi renegociada!", "bot");
-        pushMessage("O protocolo da solicitação é #1382", "bot");
-        showCSat();
-      }
-
-      if (msg === "Cancelar") {
-        pushMessage("Sua renegociação foi cancelada.", "bot");
-        showCSat();
-      }
-
+    addListener(reportListener);
+    addListener(tipListener);
+    addListener(renegotiationListener);
+    addListener(satisfactionListener);
+    addListener((msg, ctx) => {
       if (msg === "Não") showCSat();
-
-      if (msg === "Estou bem, obrigado(a)") {
-        pushMessage("Então até mais, estou por aqui se precisar!", "bot");
-      }
-
-      if (msg.indexOf("Pouco satisfeito") === 0 || msg === "Indiferente") {
-        pushMessage("É uma pena ouvir isso", "bot");
-        pushMessage(
-          "Nossa equipe está trabalhando para que eu possa melhorar nossa interação por aqui.",
-          "bot"
-        );
-        finishChat();
-      }
-
-      if (msg.indexOf("Muito satisfeito") === 0) {
-        finishChat();
-      }
     });
   }, []);
 }
